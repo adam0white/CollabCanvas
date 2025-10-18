5526a80f394551e91541b36438353f5f520ddc31
Abdul Isik <abdul@abdulisik.com>
2025-10-18 16:00:52 -0500

    Refactor: Update roomId fixture to fix playwright runs

--- web/tests/e2e/fixtures.ts
+++ web/tests/e2e/fixtures.ts
@@ -40,7 +40,7 @@ export const test = base.extend<TestFixtures>({
   /**
    * Unique room ID for each test
    */
-  roomId: async (_fixtures, use) => {
+  roomId: async ({}, use) => {
     const roomId = generateRoomId();
     await use(roomId);
   },
